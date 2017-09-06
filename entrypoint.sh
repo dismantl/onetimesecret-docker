16139013197818ad77afffb9bbc45cb8a5fb243d
Dan Staples <dan@disman.tl>
2017-09-06 13:36:34 -0400

    add setting of email sender name to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [[ -n ${OTS_SECRET+x} ]]; then
   sed "s/:secret:.*\$/:secret: $OTS_SECRET/" /etc/onetime/config.bak > /etc/onetime/config
 elif grep -q ":secret: CHANGEME" /etc/onetime/config; then
   PASS="$(dd if=/dev/urandom bs=20 count=1 2>/dev/null| openssl sha1 | grep -o '[a-f0-9]\{40\}')"
-  echo "Setting OTS secret to $PASS"
+  echo "Generating OTS secret: $PASS"
   cp /etc/onetime/config{,.bak}
   sed "s/:secret:.*\$/:secret: $PASS/" /etc/onetime/config.bak > /etc/onetime/config
 fi
@@ -18,11 +18,19 @@ fi
 if grep -q "redis://user:CHANGEME@127.0.0.1:7179" /etc/onetime/config && \
     grep -q "requirepass CHANGEME" /etc/onetime/redis.conf; then
   PASS="$(dd if=/dev/urandom bs=20 count=1 2>/dev/null| openssl sha1 | grep -o '[a-f0-9]\{40\}')"
-  echo "Setting redis password to $PASS"
+  echo "Generating Redis password: $PASS"
   cp /etc/onetime/config{,.bak}
   cp /etc/onetime/redis.conf{,.bak}
   sed "s/requirepass.*\$/requirepass $PASS/" /etc/onetime/redis.conf.bak > /etc/onetime/redis.conf
   sed "s/redis:\/\/user:CHANGEME/redis:\/\/user:$PASS/" /etc/onetime/config.bak > /etc/onetime/config
+fi
+
+# change default sender name in automated emails
+# (even better, create and mount your own email templates at the locations below)
+if [[ -n ${OTS_NAME+x} ]]; then
+  echo "Setting email sender name to $OTS_NAME"
+  sed -i.bak "s/Delano/$OTS_NAME/" /var/lib/onetime/templates/email/password_request.mustache
+  sed -i.bak "s/Delano/$OTS_NAME/" /var/lib/onetime/templates/email/welcome.mustache
 fi
 
 redis-server /etc/onetime/redis.conf
